65fe2b3a30d121c6ddc2a07e1fdff98cd2cacd13
poorvisha26 <34398601+poorvisha26@users.noreply.github.com>
2018-01-02 22:38:21 +0530

    Create java2.java

---
+++ java2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class java2
+{
+public static void main(String s[])
+{
+int a;
+Scanner sc=new Scanner(System.in)
+a=sc.nextInt();
+if(a%2==0)
+System.out.println("even");
+else
+System.out.println("odd");
+}
+}
