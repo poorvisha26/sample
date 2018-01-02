d5bee974c014854cc4c9e6df8f74d6ff8d50620a
poorvisha26 <34398601+poorvisha26@users.noreply.github.com>
2018-01-02 22:44:12 +0530

    Create java3.java

---
+++ java3.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class java3
+{
+public static void main(String s[])
+{
+String s;
+Scanner sc=new Scanner(System.in)
+s=sc.newInt();
+if(s=='a'&&s=='e'&&s=='i'&&s=='o'&&s=='u'||s=='A'&&s=='E'&&s=='I'&&s=='O'&&s=='U')
+System.out.print("vowel");
+else
+System.out.print("consonant");
+}
+}
