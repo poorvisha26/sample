8f2e48723b727e82f2f3c638a31f15612c303631
poorvisha26 <34398601+poorvisha26@users.noreply.github.com>
2018-01-02 23:38:03 +0530

    Create java7.java

---
+++ java7.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class java7
+{
+public static void main(String s[])
+{
+int i,n;
+String s;
+Scanner sc=new Scanner(System.in)
+s=sc.nextLine();
+n=sc.nextInt();
+for(i=0;i<n;i++)
+{
+System.out.print(s);
+}
+}
+}
