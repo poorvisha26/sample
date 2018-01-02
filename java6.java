5a217251ea83df3fd1a43955169a4c76db1b8133
poorvisha26 <34398601+poorvisha26@users.noreply.github.com>
2018-01-02 23:15:30 +0530

    Create java6.java

---
+++ java6.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class java6
+{
+public static void main(String s[])
+{
+int a;
+Scanner sc=new Scanner(System.in)
+a=sc.nextInt();
+if(a%4==0)
+System.out.print("leap year");
+else
+System.out.print("no");
+}
+}
