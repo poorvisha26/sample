0fb328f839c3a9a569cc49cdd199f5ec090346c3
poorvisha26 <34398601+poorvisha26@users.noreply.github.com>
2018-01-02 23:45:47 +0530

    Create java9.java

---
+++ java9.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class java9
+{
+public static void main(String s[])
+{
+int i,n,s,sum=0;
+Scanner sc=new Scanner(System.in)
+n=sc.nextInt();
+s=sc.nextInt();
+for(i=0;i<n;i++)
+{
+System.out.print(i);
+}
+for(i=0;i<=s;i++)
+{
+sum+=i;
+}
+}
+}
