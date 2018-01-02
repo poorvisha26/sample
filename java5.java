bbc6851c1f05d11e7a887830be4fa12f81e7b6e7
poorvisha26 <34398601+poorvisha26@users.noreply.github.com>
2018-01-02 23:09:45 +0530

    Create java5.java

---
+++ java5.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class java5
+{
+public static void main(String s[])
+{
+int a,b,c,d;
+Scanner sc=new Scanner(String s[])
+d=a>b?(a>c?a:c):(b>c?b:c);
+System.out.print(d);
+}
+}
