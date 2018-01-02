341d38190f69e47f4c974000cd27d466bbaeb2ea
poorvisha26 <34398601+poorvisha26@users.noreply.github.com>
2018-01-02 23:51:56 +0530

    Create java10.java

---
+++ java10.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class java10
+{
+public static void main(String s[])
+{
+int i,c=0,s,n;
+Scanner sc=new Scanner(System.in)
+s=sc.nextInt();
+while(s>0)
+{
+n=s%10;
+s=s/10;
+c++;
+}
+}
+}
