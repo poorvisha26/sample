8a08ee77a24801b668bd70d332fcd359430f86c2
poorvisha26 <34398601+poorvisha26@users.noreply.github.com>
2018-01-02 23:41:27 +0530

    Create java8.java

---
+++ java8.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class java8
+{
+public static void main(String s[])
+{
+int i,n,sum=0;
+Scanner sc=new Scanner(System.in)
+n=sc.nextInt();
+for(i=0;i<=n;i++)
+{
+sum+=i;
+}
+}
+}
