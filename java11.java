4e600a830437cc1f8dbdef8e76fe3bee1196651b
poorvisha26 <34398601+poorvisha26@users.noreply.github.com>
2018-04-03 20:12:15 +0530

    java11.java

---
+++ java11.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class power
+{
+public static void main(String s[])
+{
+double in1,in2,res;
+Scanner sc=new Scanner(System.in);
+in1=sc.nextDouble();
+in2=sc.nextDouble();
+System.out.println(in1);
+System.out.println(in2);
+res=Math.pow(in1,in2);
+System.out.println(res);
+}
+}
