0ed48483e6ec0d28b7dff97edf5a1204397589a1
poorvisha26 <34398601+poorvisha26@users.noreply.github.com>
2018-04-20 19:24:03 +0530

    Create java16.java

---
+++ java16.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class Java16
+{
+public static void main(String s[])
+{
+int n,i,odd;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+for(i=0;i<=n;i++)
+{
+if(i%2==0)
+System.out.println(i);
+}
+}
+}
