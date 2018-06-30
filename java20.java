12b19df6a9051dbb2d2fd2e3ad2839c947938d4d
poorvisha26 <34398601+poorvisha26@users.noreply.github.com>
2018-06-30 22:43:16 +0530

    Create java20.java

---
+++ java20.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class java20
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int mul=0;
+for(int i=1;i<=n;i++)
+{
+mul+=n;
+System.out.print(mul+" ");
+}
+}
+}
