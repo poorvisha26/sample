209319cf7a759a2a7de8d4178fd75ba8a08fc2c1
poorvisha26 <34398601+poorvisha26@users.noreply.github.com>
2018-07-01 14:36:28 +0530

    Create java28.java

---
+++ java28.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+public class java22
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<a.length;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.println(a[i]+" "+i);
+        }
+    }
+}
+
+
+
+
