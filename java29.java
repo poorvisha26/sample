6963d58d33ab05dd6a4c10b02d610c3793784dce
poorvisha26 <34398601+poorvisha26@users.noreply.github.com>
2018-07-01 14:54:43 +0530

    Create java29.java

---
+++ java29.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class java29
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int h=n/60;
+        int s=n*60;
+        System.out.print(h+" "+n);
+    }
+}
+
+
+
+
